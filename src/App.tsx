fb8f46f54ca85ead56432416a0d0e0682d1a044f
K.Utsunomiya <kii.utsunomiya@gmail.com>
2020-12-31 17:33:39 +0900

    LadingContextを用意

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React, { ReactElement } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { LoadingProvider } from './contexts/LoadingContext';
 import { AuthenticationProvider } from './contexts/AuthenticationContext';
 import Header from './layouts/Header';
 import Main from './layouts/Main';
 import Footer from './layouts/Footer';
-import SwitchRoute from './layouts/SwitchRoute';
 
 const App: React.FC = (): ReactElement => {
   return (
     <AuthenticationProvider>
-      <BrowserRouter>
-        <Header />
-        <Main>
-          <SwitchRoute />
-        </Main>
-        <Footer />
-      </BrowserRouter>
+      <LoadingProvider>
+        <BrowserRouter>
+          <Header />
+          <Main />
+          <Footer />
+        </BrowserRouter>
+      </LoadingProvider>
     </AuthenticationProvider>
   );
 }
