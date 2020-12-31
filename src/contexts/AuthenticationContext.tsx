3433ca0d882b6db14c282e3e9ba2f43efa493bad
K.Utsunomiya <kii.utsunomiya@gmail.com>
2020-12-31 15:52:47 +0900

    認証周りの実装

---
+++ src/contexts/AuthenticationContext.tsx
@@ -0,0 +1,56 @@
+import React, { createContext, useState, useEffect, useCallback, ReactElement } from 'react';
+import firebase from '../firebase';
+
+type Props = {
+  children?: React.ReactNode;
+}
+
+type Account = {
+  uid: string;
+}
+
+type Context = {
+  account: Account | null;
+  signIn: () => void;
+  signOut: () => void;
+}
+
+export const AuthenticationContext: React.Context<Context> = createContext<Context>({
+  account: null,
+  signIn: () => null,
+  signOut: () => null,
+});
+
+export const AuthenticationProvider: React.FC<Props> = (props: Props): ReactElement => {
+  const [account, setAccount] = useState<Account | null>(null);
+
+  const signIn: () => void = useCallback(() => {
+    firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+    firebase.auth().getRedirectResult().then(result => {
+      if (result.user) {
+        setAccount({ uid: result.user.uid });
+      }
+    });
+  }, []);
+
+  const signOut: () => void = useCallback(() => {
+    firebase.auth().signOut();
+  }, []);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        setAccount({ uid: user.uid });
+      }
+      else {
+        setAccount(null);
+      }
+    });
+  });
+
+  return (
+    <AuthenticationContext.Provider value={{ account, signIn, signOut }}>
+      {props.children}
+    </AuthenticationContext.Provider>
+  );
+}
