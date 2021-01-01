25394115c92e88df857caa29fcc1c0374642855d
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-01-01 16:37:04 +0900

    サインイン・サインアウトのイベントハンドラをuseCallbackで宣言

--- src/layouts/Navigation.tsx
+++ src/layouts/Navigation.tsx
@@ -1,15 +1,23 @@
-import React, { ReactElement, useContext } from 'react';
+import React, { ReactElement, useContext, useCallback } from 'react';
 import { AuthenticationContext } from '../contexts/AuthenticationContext';
 import '../styles/layouts/navigation.scss';
 
 const Navigation: React.FC = (): ReactElement => {
   const { account, signIn, signOut } = useContext(AuthenticationContext);
 
+  const handleClick = useCallback(() => {
+    if (account) {
+      signOut();
+    } else {
+      signIn();
+    }
+  }, [account])
+
   return (
     <nav role="navigation" aria-label="header-navigation" className='navigation'>
       <ul className="navigation__actions">
         <li className="navigation__action">
-          <button onClick={() => account ? signOut() : signIn()} className="navigation__link">
+          <button onClick={handleClick} className="navigation__link">
             {account ? 'サインアウト' : 'サインイン'}
           </button>
         </li>
