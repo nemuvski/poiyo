3ab18f9bce9703b6f27017fc0bb6e0f377e1ef46
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-01-02 22:25:38 +0900

    利用規約とプライバシーポリシーのページを用意

---
+++ src/screens/Privacy.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+const Privacy: React.FC = (): ReactElement => {
+  return (
+    <article>
+      <h1>プライバシーポリシー</h1>
+    </article>
+  );
+}
+
+export default Privacy;
