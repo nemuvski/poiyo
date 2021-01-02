b020d01ef9a6383803866c5888967115df0731ce
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-01-02 22:18:52 +0900

    フッターにリンク追加

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,11 +1,24 @@
 import React, { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.svg';
 import '../styles/layouts/footer.scss';
 
 const Footer: React.FC = (): ReactElement => {
   return (
     <footer className="footer">
       <div className="footer__inner">
-        <span>© 2021 Poiyo</span>
+        <div className="footer__container">
+          <div className="footer__logo">
+            <Link to="/"><img src={logo} alt="Poiyo" /></Link>
+          </div>
+          <nav className="footer__navigation">
+            <ul>
+              <li><Link to="/terms">利用規約</Link></li>
+              <li><Link to="/privacy">プライバシーポリシー</Link></li>
+            </ul>
+          </nav>
+        </div>
+        <p className="footer__copyright">© 2021 Poiyo</p>
       </div>
     </footer>
   );
