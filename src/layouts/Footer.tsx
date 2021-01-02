fa6f99c730e6d8839e4f3792d01bd8e91907cead
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-01-02 23:03:57 +0900

    コレ棚へのリンクを追加

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.svg';
+import koredanaLogo from '../assets/koredana.svg';
 import '../styles/layouts/footer.scss';
 
 const Footer: React.FC = (): ReactElement => {
@@ -8,13 +9,20 @@ const Footer: React.FC = (): ReactElement => {
     <footer className="footer">
       <div className="footer__inner">
         <div className="footer__container">
-          <div className="footer__logo">
+          <div className="footer__brand">
             <Link to="/"><img src={logo} alt="Poiyo" /></Link>
+            <p>「ぽいよ」な情報共有コミュニティ</p>
           </div>
           <nav className="footer__navigation">
             <ul>
               <li><Link to="/terms">利用規約</Link></li>
               <li><Link to="/privacy">プライバシーポリシー</Link></li>
+              <li>
+                <a href="https://koredana.info/" target="_blank" rel="noreferrer noopener">
+                  <img src={koredanaLogo} alt="コレ棚" className="footer__koredana-logo" />
+                  管理人のブログ
+                </a>
+              </li>
             </ul>
           </nav>
         </div>
