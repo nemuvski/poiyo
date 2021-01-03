47834bfa06f774f823721dcdc3ae86163ddfa322
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-01-03 20:00:01 +0900

    概要文とサインイン部分の用意

--- src/screens/Front.tsx
+++ src/screens/Front.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement, useContext, useCallback } from 'react';
+import { Link } from 'react-router-dom';
 import { AuthenticationContext } from '../contexts/AuthenticationContext';
+import ArticleInner from '../components/ArticleInner';
 import keyVisual from '../assets/key-visual.svg';
 import googleIcon from '../assets/google-icon.svg';
 import '../styles/screens/page-front.scss';
@@ -12,42 +14,40 @@ const Front: React.FC = (): ReactElement => {
   }, []);
 
   return (
-    <div className="page-front">
-      <section className="page-front__section">
-        <h2>「ぽいよ」な情報共有コミュニティ</h2>
-        <div className="page-front__container">
-          <div>
-            <p>
-              話題ごとにスレッドをたてて、知っていることや感想・意見などを投稿しましょう！
-            </p>
+    <ArticleInner>
+      <div className="page-front">
+        <section className="page-front__section">
+          <div className="page-front__container">
+            <div className="page-front__summary">
+              <p><strong>Poiyo</strong>（ぽいよ）は1つの話題ごとに、みんなでコメントし合うコミュニティサービスです。</p>
+              <p>「ちょっと知っていること」や「これってどういうことなんだろう」といったことなどを投稿し、みんなからコメントをもらうことで知見を広げていきましょう。</p>
+            </div>
+            <img src={keyVisual} className="page-front__key-visual" />
           </div>
-          <img src={keyVisual} className="page-front__key-visual" />
-        </div>
-      </section>
-      <section className="page-front__section">
-        <h2>特徴</h2>
-        <ul>
-          <li>匿名</li>
-          <li>スレッド単位</li>
-        </ul>
-      </section>
-      <section className="page-front__section">
-        <h2>はじめかたは簡単！</h2>
-        <p>
-          Googleアカウントでご利用できます。
-        </p>
-        <p>
-          新規作成、サインインのどちらも以下のボタンから行えます。<br />
-          利用規約、プライバシーポリシーに同意した上でサインインしてください。
-        </p>
-        <div className="page-front__signin-wrapper">
-          <button onClick={handleSignIn} className="is-white page-front__signin-button">
-            <img src={googleIcon} alt="Google" />
-            <span>Googleアカウントでサインイン</span>
-          </button>
-        </div>
-      </section>
-    </div>
+        </section>
+        <section className="page-front__section">
+          <h2>はじめかたは簡単！</h2>
+          <p>
+            Googleアカウントでご利用できます。
+            新規作成、サインインのどちらも以下のボタンから行えます。<br />
+            <Link to="/privacy">プライバシーポリシー</Link>、<Link to="/terms">利用規約</Link>に同意した上でサインインしてください。
+          </p>
+          <div className="page-front__signin-wrapper">
+            <button onClick={handleSignIn} className="is-white page-front__signin-button">
+              <img src={googleIcon} alt="Google" />
+              <span>Googleでサインイン</span>
+            </button>
+          </div>
+        </section>
+        <section className="page-front__section">
+          <h2>特徴</h2>
+          <ul>
+            <li>匿名</li>
+            <li>ボード単位</li>
+          </ul>
+        </section>
+      </div>
+    </ArticleInner>
   );
 }
 
