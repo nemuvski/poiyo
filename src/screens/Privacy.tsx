1c56bedc147f34a26dbe11a157092c138347fceb
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-01-14 23:39:15 +0900

    タイトルの設定

--- src/screens/Privacy.tsx
+++ src/screens/Privacy.tsx
@@ -1,6 +1,11 @@
-import React, { ReactElement } from 'react';
+import React, {ReactElement, useEffect} from 'react';
+import {setDocumentTitle} from "../utilities/DocumentTitle";
 
 const Privacy: React.FC = (): ReactElement => {
+  useEffect(() => {
+    setDocumentTitle('プライバシーポリシー');
+  }, []);
+
   return (
     <article>
       <h1>プライバシーポリシー</h1>
