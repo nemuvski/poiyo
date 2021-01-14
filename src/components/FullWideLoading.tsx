8924b6a38b279b4675e4381f7b020c2a4077ff93
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-01-14 22:58:39 +0900

    画面全体のローディングコンポーネント

---
+++ src/components/FullWideLoading.tsx
@@ -0,0 +1,10 @@
+import React, {ReactElement} from 'react';
+import '../styles/components/full-wide-loading.scss';
+
+const FullWideLoading: React.FC = (): ReactElement => {
+  return (
+    <div className="full-wide-loading" />
+  )
+}
+
+export default FullWideLoading;
