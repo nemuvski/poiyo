8cc412ac903c4834b1255669dcc5ab8dcf008297
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-01-23 16:36:45 +0900

    サービス関数にコメント記述

--- src/libs/services/AuthService.ts
+++ src/libs/services/AuthService.ts
@@ -6,6 +6,14 @@ import AuthApi from '../api/AuthApi';
 import {Account} from '../models/Account';
 import {AuthRequest} from "../models/Auth";
 
+/**
+ * 認証してアカウント情報を取得する.
+ *
+ * @param token トークン.
+ * @param email メールアドレス.
+ * @param serviceId サービスのID.
+ * @param serviceType サービスの種別.
+ */
 const auth = (token: string, email: string | null, serviceId: string, serviceType = 'ggl'): Promise<Account> => {
   const authRequest: AuthRequest = {
     serviceType,
--- src/utilities/DocumentTitle.ts
+++ src/utilities/DocumentTitle.ts
@@ -1,7 +1,9 @@
 /**
- * タイトルを変更するユーティリティ.
+ * タイトルを設定する.
+ *
+ * @param title タイトル.
+ * @param setSiteTitle サイトタイトルを末尾に付与したものにするかのフラグ.
  */
-
 export const setDocumentTitle = (title: string, setSiteTitle = true): void => {
   document.title = setSiteTitle ? `${title} - Poiyo` : title;
 };
