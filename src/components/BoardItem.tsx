59ecfbdf717d77124dc5a47b72ce4ead4c242150
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-02-01 00:08:39 +0900

    自分のボードを表すアイコンを表示する

--- src/components/BoardItem.tsx
+++ src/components/BoardItem.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Link} from 'react-router-dom';
 import {Board, BoardLocationState} from "../libs/models/Board";
 import Dayjs, {formatYMD} from '../libs/common/Dayjs';
 import '../styles/components/board-item.scss';
+import clsx from "clsx";
+import {AuthenticationContext} from "../contexts/AuthenticationContext";
 
 type Props = {
   board: Board;
 };
 
 const BoardItem: React.FC<Props> = (props: Props) => {
+  const { account } = useContext(AuthenticationContext);
+
   return (
     <div className="board-item">
       <Link
@@ -16,7 +20,7 @@ const BoardItem: React.FC<Props> = (props: Props) => {
           pathname: `/board/${props.board.boardId}`,
           state: {board: props.board} as BoardLocationState,
         }}
-        className="board-item__link"
+        className={clsx(['board-item__link', {'is-mine':(account && account.id && account.id == props.board.ownerAccountId)}])}
       >
         <span className="board-item__title">{props.board.title}</span>
         <time className="board-item__date">
