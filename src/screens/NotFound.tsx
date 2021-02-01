62b20a31879f4b5666f765166ef187fc5e573453
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-02-01 23:26:29 +0900

    NotFoundの内容を変更

--- src/screens/NotFound.tsx
+++ src/screens/NotFound.tsx
@@ -1,19 +1,37 @@
-import React, {useEffect} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {setDocumentTitle} from "../utilities/DocumentTitle";
-import {Link} from "react-router-dom";
+import {useHistory} from "react-router-dom";
 import ArticleInner from "../components/ArticleInner";
+import {AuthenticationContext} from "../contexts/AuthenticationContext";
+import notFound from '../assets/not-found.svg';
+import '../styles/screens/page-not-found.scss';
 
 const NotFound: React.FC = () => {
+  const { account } = useContext(AuthenticationContext);
+  const history = useHistory();
+
   useEffect(() => {
     setDocumentTitle('ページが見つかりません');
   }, []);
 
   return (
-    <ArticleInner>
-      <h1>お探しのページが見つかりませんでした。</h1>
-      <p>お探しのページは一時的にアクセスできない状況か、移動もしくは削除された可能性があります。</p>
-      <Link to="/">トップページへ</Link>
-    </ArticleInner>
+    <div className="page-not-found">
+      <ArticleInner>
+        <h1>お探しのものが見つかりませんでした。</h1>
+        <p className="page-not-found__container">
+          <img className="page-not-found__image" alt="何も見つかりませんでした。" src={notFound} />
+        </p>
+        <p className="page-not-found__container">
+          探してみましたが、見つかりませんでした。<br />
+          一時的にアクセスできない状況か、移動もしくは削除された可能性があります。
+        </p>
+        <p className="page-not-found__container">
+          <button type="button" onClick={() => history.push(!account ? '/' : '/dashboard')}>
+            {!account ? 'トップページへ' : 'ダッシュボードへ'}
+          </button>
+        </p>
+      </ArticleInner>
+    </div>
   );
 }
 
