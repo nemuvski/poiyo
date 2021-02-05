65e23c4aebb6c052781ad0a65dc7b21b56001d7b
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-02-05 10:08:40 +0900

    削除ボタンのイベントとローディングエフェクトを用意

--- src/screens/EditBoard.tsx
+++ src/screens/EditBoard.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {useHistory, useLocation} from "react-router-dom";
 import {setDocumentTitle} from "../utilities/DocumentTitle";
 import ArticleInner from "../components/ArticleInner";
@@ -6,11 +6,16 @@ import ArticleSection from "../components/ArticleSection";
 import BoardForm from "../components/BoardForm";
 import {Board, BoardLocationState} from "../libs/models/Board";
 import '../styles/screens/page-edit-board.scss';
+import BoardsService from "../libs/services/BoardsService";
+import {AuthenticationContext} from "../contexts/AuthenticationContext";
+import FullWideLoading from "../components/FullWideLoading";
 
 const EditBoard: React.FC = () => {
   const location = useLocation<BoardLocationState>();
   const history = useHistory();
   const [board, setBoard] = useState<Board | undefined>();
+  const { account } = useContext(AuthenticationContext);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setDocumentTitle('ボード編集・削除');
@@ -24,14 +29,29 @@ const EditBoard: React.FC = () => {
   }, []);
 
   const handleClick = () => {
+    if (!account || !board) {
+      console.error('問題が発生したため、処理を中断しダッシュボードへ遷移します。');
+      history.replace('/dashboard');
+      return;
+    }
     if (!confirm('ボードを削除しますがよろしいですか？')) {
       return;
     }
-    // 削除する処理.
+    setLoading(true);
+    BoardsService.remove(account.token, board.boardId)
+      .catch(error => {
+        console.error(error);
+      })
+      .finally(() => {
+        history.replace('/dashboard');
+        setLoading(false);
+      });
   };
 
   return (
     <ArticleInner>
+      {loading && <FullWideLoading />}
+
       <div className="page-edit-board">
         <h1>ボード編集・削除</h1>
         <ArticleSection wider={true}>
