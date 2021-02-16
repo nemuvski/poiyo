da4f3e643a884b9a4def66c77da6838bf8ac68a8
K.Utsunomiya <kii.utsunomiya@gmail.com>
2021-02-16 11:05:36 +0900

    v1.0.1

--- src/libs/services/NewsService.ts
+++ src/libs/services/NewsService.ts
@@ -4,7 +4,8 @@
 import {News} from "../models/News";
 
 const data: Array<News> = [
-  new News('2021-02-14', '本サービスリリース開始 🎉'),
+  new News('2021-02-16', '🛠 ボードまたはコメント削除時とサービス退会時にモーダルを表示'),
+  new News('2021-02-14', '🎉 本サービスリリース開始'),
 ];
 
 export default { data };
